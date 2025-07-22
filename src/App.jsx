605f720381f7189f74550fe86747c50154a38f56
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-22 19:20:06 +0800

    Added paragraphs to body2

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,22 @@ function App() {
       <header>
         <div id="header" class="header">
           <img src="./public/img/mama-susans.png" class="header-photo"></img>
-          <ul class>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">MENU</a></li>
-            <li><a href="#">GALLERY</a></li>
-            <li><a href="#">FEEDBACKS</a></li>
+          <ul class="options">
+            <li><a href="#"><b>HOME</b></a></li>
+            <li><a href="#"><b>MENU</b></a></li>
+            <li><a href="#"><b>GALLERY</b></a></li>
+            <li><a href="#"><b>FEEDBACKS</b></a></li>
           </ul>
         </div>
       </header>
 
       <body>
+
         <div id="body1" class="body1">
           <img src="./public/img/cover-photo.webp"></img>
         </div>
 
         <div id="body2" class="body2">
-
           <h1 class="phrase1">WE ARE OPEN 24 HOURS, EVERYDAY!</h1>
           <h2 class="phrase2"> DINE IN - TAKE-OUT - DELIVERY</h2>
 
@@ -40,12 +40,68 @@ function App() {
             </div>
           </div>
 
+          <div>
+            <h2 class="about">Mama Susan's: From Humble Beginnings to Metro-South Favorite.... Kain po tayo!</h2>
+
+            <br></br>
+
+            <h2 class="about">Our Story</h2>
+            <p class="about">
+              In 2016, Mama Susan's emerged as a humble, yet authentic, "tapsihan" experience.
+              Nestled within the heart of BF International VIllage, Las Piñas City, our initial
+              offering was a simple four-seater counter, dedicated to serving classic Filipino
+              comfort food. The aroma of sizzling "tapa" and the warmth of our service quickly
+              resonated with the local community, fostering a loyal following.
+            </p>
+
+            <br></br>
+
+            <p class="about">
+              The overwhelming positive response fueled our ambition to grow. Recognizing the
+              demand for accessible, delicious meals at any hour, Mama Susan's transitioned to
+              24-hour operations. This expansion allowed us to cater to the diverse schedules of
+              our customers, from early morning risers to late-night diners. To accommodate the 
+              number of customers, we also expanded our dining space, creating a more comfortable
+              and inviting atmosphere.
+            </p>
+
+            <br></br>
+
+            <h2 class="about">Easy Ordering, Delivered!</h2>
+            <p class="about">
+            Now, through our online presence and partnerships with GrabFood and Foodpanda, we
+            serve even more customers. We deliver to Las Piñas City, Parañaque City, Pasay City,
+            and parts of Cavite.
+            </p>
+
+            <br></br>
+
+            <h2 class="about">Our Mission?</h2>
+            <p class="about">
+            Simply, to serve afordable delicious food!  We are committed to always strive for
+            your satisfaction, continuously improve our service, and serve delicious meals at
+            affordable prices.
+            </p>
+
+            <br></br>
+
+            <h2 class="about">What's Next?</h2>
+            <p class="about">
+            We're excited to keep growing and serving you better. Thank you for your support!
+            </p>
+            
+          </div>
+
+
+
+
         </div>
+
       </body>
 
       <footer>
         <div id="footer" class="footer">
-          <h1> &copy; All rights reserved.</h1>
+          <h6> &copy; Mama Susan's Original Tapsilog</h6>
         </div>
       </footer>
     </>
