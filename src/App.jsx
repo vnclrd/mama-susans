964377146a1333fba4808c5fc5c177891ba519b2
ivancalarde1301 <ivan.calarde1301@gmail.com>
2025-07-22 20:49:19 +0800

    Adjusted texts

--- src/App.jsx
+++ src/App.jsx
@@ -40,13 +40,16 @@ function App() {
             </div>
           </div>
 
+          <div class="mid-image">
+            <img src="images/mama-susans.png" class="mid-image-class"></img>
+            <h1 class="about-text-title">From Humble Beginnings, to Metro-South Favorite</h1>
+            <h1 class="about-text-title">Kain po tayo!</h1>
+          </div>
+          
           <div class="about">
-            <h2 class="about-text-title">Mama Susan's: From Humble Beginnings to Metro-South Favorite.... Kain po tayo!</h2>
 
-            <br></br>
-
-            <h3 class="about-text-story">Our Story</h3>
-            <p class="about-text">
+            <h1 class="about-text-story">Our Story</h1>
+            <p class="about-text-p1">
               In 2016, Mama Susan's emerged as a humble, yet authentic, "tapsihan" experience.
               Nestled within the heart of BF International VIllage, Las Piñas City, our initial
               offering was a simple four-seater counter, dedicated to serving classic Filipino
@@ -54,9 +57,7 @@ function App() {
               resonated with the local community, fostering a loyal following.
             </p>
 
-            <br></br>
-
-            <p class="about-text">
+            <p class="about-text-p2">
               The overwhelming positive response fueled our ambition to grow. Recognizing the
               demand for accessible, delicious meals at any hour, Mama Susan's transitioned to
               24-hour operations. This expansion allowed us to cater to the diverse schedules of
@@ -67,8 +68,8 @@ function App() {
 
             <br></br>
 
-            <h2 class="about-text">Easy Ordering, Delivered!</h2>
-            <p class="about-text">
+            <h1 class="about-text-easy">Easy Ordering, Delivered!</h1>
+            <p class="about-text-p3">
             Now, through our online presence and partnerships with GrabFood and Foodpanda, we
             serve even more customers. We deliver to Las Piñas City, Parañaque City, Pasay City,
             and parts of Cavite.
@@ -76,8 +77,8 @@ function App() {
 
             <br></br>
 
-            <h2 class="about-text">Our Mission?</h2>
-            <p class="about-text">
+            <h1 class="about-text-mission">Our Mission</h1>
+            <p class="about-text-p4">
             Simply, to serve afordable delicious food!  We are committed to always strive for
             your satisfaction, continuously improve our service, and serve delicious meals at
             affordable prices.
@@ -85,8 +86,8 @@ function App() {
 
             <br></br>
 
-            <h2 class="about-text">What's Next?</h2>
-            <p class="about-text">
+            <h1 class="about-text-next">What's Next?</h1>
+            <p class="about-text-p5">
             We're excited to keep growing and serving you better. Thank you for your support!
             </p>
             
